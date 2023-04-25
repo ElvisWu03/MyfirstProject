8c5d825ec370b4e260b2a0cfa874638e9bca86ae
ElvisWu03 <131748568+ElvisWu03@users.noreply.github.com>
2023-04-25 15:16:09 +0800

    Add files via upload

---
+++ SC001_lecture02/DoubleBeepers.py
@@ -0,0 +1,67 @@
+"""
+File: DoubleBeepers.py
+Name:ElvisWu
+-------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel will double the beepers
+    """
+    move()
+    double_beepers()
+    put_beepers_back()
+    karel_goes_home()
+
+
+def double_beepers():
+    while on_beeper():
+        #old beepers, facing East
+        pick_beeper()
+        move()
+        put_beeper()
+        put_beeper()
+        #new beepers, facing East
+        turn_around()
+        move()
+        #old beepers, facing West
+        turn_around()
+
+
+def put_beepers_back():
+    move()
+    while on_beeper():
+        turn_around()
+        pick_beeper()
+        move()
+        put_beeper()
+        turn_around()
+        move()
+
+
+
+
+def karel_goes_home():
+    turn_around()
+    move()
+    move()
+
+
+def turn_around():
+    for i in range(2):
+        turn_left()
+
+
+
+
+
+
+
+
+# ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
